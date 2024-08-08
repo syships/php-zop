e1a55827c8356485b12665f12cce1600f906c084
syships <101991678+syships@users.noreply.github.com>
2024-08-08 18:08:52 +0800

    Add files via upload

---
+++ src/Application.php
@@ -0,0 +1,36 @@
+<?php
+namespace syships\qiniu;
+
+use Qiniu\Auth;
+use yii\base\Component;
+use yii\base\InvalidConfigException;
+
+/**
+ * 
+ */
+class Application extends Component
+{
+    public $accessKey;
+
+    public $secretKey;
+
+    public $options;
+
+    public function init()
+    {
+        parent::init();
+        if ($this->accessKey === null) {
+            throw new InvalidConfigException('Qiniu::accessKey must be set.');
+        }
+        if ($this->secretKey === null) {
+            throw new InvalidConfigException('Qiniu::secretKey must be set.');
+        }
+    }
+
+    /**
+     * @return Auth
+     */
+    public function Auth(){
+        return new Auth($this->accessKey, $this->secretKey, $this->options);
+    }
+}
